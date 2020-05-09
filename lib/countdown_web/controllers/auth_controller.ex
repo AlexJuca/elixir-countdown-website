fa043c1f634248f1e5fd56826be6fb2b43f8517c
Alexande Juca <corextechnologies@gmail.com>
2020-05-09 16:01:57 +0100

    Fixed bug that causes failure to login via o-auth

--- lib/countdown_web/controllers/auth_controller.ex
+++ lib/countdown_web/controllers/auth_controller.ex
@@ -13,13 +13,7 @@ defmodule CountdownWeb.AuthController do
       |> redirect(to: "/")
     end
 
-    def callback(%{assigns: %{ ueberauth_failure: fails}}, conn) do
-      conn
-      |> put_flash(:error, "Failed to authenticate.")
-      |> redirect(to: "/")
-    end
-
-    def callback(%{ assigns: %{ ueber_auth: auth} }, conn) do
+    def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
       case UserFromAuth.find_or_create(auth) do
         {:ok, user} -> 
           conn
@@ -31,5 +25,11 @@ defmodule CountdownWeb.AuthController do
             |> put_flash(:error, reason)
             |> redirect(to: "/")
       end
+    end
+
+    def callback(%{assigns: %{ueberauth_failure: _fails}} = conn, _params) do
+      conn
+      |> put_flash(:error, "Failed to authenticate.")
+      |> redirect(to: "/")
     end
 end
